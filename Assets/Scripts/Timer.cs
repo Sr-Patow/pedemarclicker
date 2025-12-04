1b35a1c9919374931317aa3d1bcf9308e792afcc
ARTHUR WILD BULIN <arthur_bulin@estudante.sesisenai.org.br>
2025-12-04 11:26:40 -0300

    correção de bugs no codigo

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,8 +6,22 @@ public class Timer : MonoBehaviour
     [SerializeField] TextMeshProUGUI textoTimer;
     [SerializeField] float tempoRestante;
 
+
+    public string telaPraEsconder = "FimDeJogo";
+
     public bool acabou = false;
     // Update is called once per frame
+
+
+
+    void Start()
+    {
+        Transform filho = transform.Find(telaPraEsconder);
+        filho.gameObject.SetActive(false);
+
+        Time.timeScale = 1;
+        acabou = false;
+    }
     void Update()
     {
         if (tempoRestante > 0)
@@ -20,11 +34,14 @@ public class Timer : MonoBehaviour
             tempoRestante = 0;
             //cabou o jogo
             textoTimer.color = Color.yellow;
-            acabou = true; 
+            acabou = true;
             Time.timeScale = 0;
+            Transform filho = transform.Find(telaPraEsconder);
+            filho.gameObject.SetActive(true);
         }
         int minutos = Mathf.FloorToInt(tempoRestante / 60);
         int segundos = Mathf.FloorToInt(tempoRestante % 60);
         textoTimer.text = string.Format("{0:00}:{1:00}", minutos, segundos);
+
     }
 }
