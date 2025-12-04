77cbd809319ff0737a2145b78544507ab79e6083
ARTHUR WILD BULIN <arthur_bulin@estudante.sesisenai.org.br>
2025-12-04 09:05:10 -0300

    adicionado timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textoTimer;
+    [SerializeField] float tempoRestante;
+    // Update is called once per frame
+    void Update()
+    {
+        if (tempoRestante > 0)
+        {
+            tempoRestante = tempoRestante - Time.deltaTime;
+        }
+
+        else
+        {
+            tempoRestante = 0;
+            //cabou o jogo
+            textoTimer.color = Color.yellow;
+            Time.timeScale = 0;
+        }
+        int minutos = Mathf.FloorToInt(tempoRestante / 60);
+        int segundos = Mathf.FloorToInt(tempoRestante % 60);
+        textoTimer.text = string.Format("{0:00}:{1:00}", minutos, segundos);
+    }
+}
